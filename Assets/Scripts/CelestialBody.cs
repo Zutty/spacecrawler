1d952475574d85723f3d33cc7df5e3e04bd8ff83
Zutty <george.weller@gmail.com>
2017-05-14 16:28:23 +0100

    Initial commit. Based on mobile example project.

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class CelestialBody : MonoBehaviour {
+	public float mass;
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public const float G = 6.673E-11f;
+
+	public CelestialBody centerBody;
+	public float semiMajorAxis;
+	public float eccentricity = 1f;
+	public float argumentOfPeriapsis;
+
+
+	void OnValidate() {
+		eccentricity = Mathf.Max(eccentricity, 0f);
+	}
+
+	void Update () {
+		float k = G * centerBody.mass;
+
+		if(Mathf.Approximately(eccentricity, 1f)) {
+			// Parabolic
+		} else if(eccentricity < 1f) {
+			// Elliptic / circular
+		} else {
+			// Hyperbolic
+		}
+
+		float parameter = semiMajorAxis * (1f - eccentricity * eccentricity);
+		float distance = parameter / (1f - eccentricity * Mathf.Cos(Time.time - argumentOfPeriapsis));
+
+		float a3 = semiMajorAxis * semiMajorAxis * semiMajorAxis;
+		float orbitalPeriod = 2f * Mathf.PI * Mathf.Sqrt(a3 / k);
+			
+		Vector3 newpos = centerBody.transform.position;
+
+		newpos.x += distance * Mathf.Cos(Time.time);
+		newpos.z += distance * Mathf.Sin(Time.time);
+
+		transform.position = newpos;
+	}
+}
